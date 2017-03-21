7dac2bed9ca11f53e4a24fcd833f9bd87d3a3880
mmeigooni <mmeigooni@gmail.com>
2017-03-21 17:41:07 -0500

    another one

--- my.conf.js
+++ my.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '/dist/coolLibrary.js , /tests/checkDivAttribute.js , /tests/checkDivBg.js'
+      '/dist/coolLibrary.js , ./tests/checkDivAttribute.js , ./tests/checkDivBg.js'
     ],
 
 
